532b610b4f03d9ad15f216dbd320c3500f6b34c0
Juan Mac <xamarin@Mac-de-xamarin.local>
2019-08-21 14:12:21 -0700

    Corregidas sombras en frame IOS

---
+++ Clicar/Clicar.iOS/Customs/CustomFrameIOS.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using Clicar.iOS.Customs;
+using CoreGraphics;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Frame), typeof(CustomFrameIOS))]
+namespace Clicar.iOS.Customs
+{
+    public class CustomFrameIOS: FrameRenderer
+    {
+
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Frame> e)
+        {
+            base.OnElementChanged(e);
+            var elem = this.Element;
+            if (elem != null)
+            {
+
+                // Border
+                this.Layer.CornerRadius = (float)elem.CornerRadius;
+
+                // Shadow
+                this.Layer.ShadowColor = UIColor.DarkGray.CGColor;
+                this.Layer.ShadowOpacity = 0.6f;
+                this.Layer.ShadowRadius = 2.0f;
+                this.Layer.ShadowOffset = new SizeF(0, 0);
+                //this.Layer.MasksToBounds = true;
+
+            }
+        }
+
+    }
+}
--- Clicar/Clicar.iOS/Customs/CustomPickerIOS.cs
+++ Clicar/Clicar.iOS/Customs/CustomPickerIOS.cs
@@ -21,7 +21,7 @@ namespace Clicar.iOS.Customs
 
                 Control.BorderStyle = UITextBorderStyle.None;
                 Control.BackgroundColor = UIKit.UIColor.FromRGBA(0,0,0,0);
-                Control.Frame = new CGRect(x: 0, y: 0, width: Frame.Width, height: 40);
+                Control.Frame = new CGRect(x: 0, y: 0, width: Frame.Width, height: 60);
 
 
             }
