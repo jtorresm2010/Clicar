09954ade15ee9f89e3091687e9fdae246c6e18b3
jtorresm2010 <jtorresm2010@gmail.com>
2019-08-21 11:36:32 -0400

    Corregidos detalles de Entry
    en login, pass recovery popup e inspeccion popup

--- Clicar/Clicar.Android/Customs/CustomEntryAndroid.cs
+++ Clicar/Clicar.Android/Customs/CustomEntryAndroid.cs
@@ -23,17 +23,17 @@ namespace Clicar.Droid.Customs
             if (e.OldElement == null)
             {
 
-                //var gradientDrawable = new GradientDrawable();
+                var gradientDrawable = new GradientDrawable();
                 //gradientDrawable.SetBounds(10, 0, 0, 2);
-                ////gradientDrawable.SetStroke(3, Android.Graphics.Color.Argb(128, 117, 171, 64));
-                //gradientDrawable.SetColor(Android.Graphics.Color.Argb(128, 117, 171, 64));
+                //gradientDrawable.SetStroke(3, Android.Graphics.Color.Argb(128, 117, 171, 64));
+                gradientDrawable.SetColor(Android.Graphics.Color.Transparent);
 
                 
-                //Control.SetBackground(gradientDrawable);
+                Control.SetBackground(gradientDrawable);
 
-                Control.Background.SetColorFilter(Android.Graphics.Color.Argb(255, 117, 171, 64), PorterDuff.Mode.SrcAtop);
+                //underline Control.Background.SetColorFilter(Android.Graphics.Color.Argb(255, 117, 171, 64), PorterDuff.Mode.SrcAtop);
                 Control.SetHintTextColor(Android.Graphics.Color.Argb(128, 117, 171, 64));
-                Control.SetPadding(100, Control.PaddingTop, 115, 30);
+                //padding Control.SetPadding(100, Control.PaddingTop, 115, 30);
             }
         }
 
