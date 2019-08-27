6eb2919b8d24bdd6d390e749734481c1d9f80b39
jtorresm2010 <jtorresm2010@gmail.com>
2019-08-27 17:34:46 -0400

    Terminado star rating V1

---
+++ Clicar/Clicar/Behaviors/RatingsBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Clicar.Behaviors
+{
+    public class RatingsBehavior : Behavior<View>
+    {
+
+
+        protected override void OnAttachedTo(View entry)
+        {
+            base.OnAttachedTo(entry);
+
+            ((Editor)entry).TextChanged += Entry_TextChanged;
+
+
+            // Perform setup
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double result;
+            bool isValid = double.TryParse(e.NewTextValue, out result);
+            ((Editor)sender).TextColor = isValid ? Color.Default : Color.Red;
+
+            ((Editor)sender).FontSize = ((Editor)sender).Text.Length;
+        }
+
+        protected override void OnDetachingFrom(View entry)
+        {
+            base.OnDetachingFrom(entry);
+            ((Editor)entry).TextChanged -= Entry_TextChanged;
+            // Perform clean up
+        }
+
+
+
+    }
+}
---
+++ Clicar/Clicar/Templates/StarTemplate.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Clicar.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StarTemplate : StackLayout
+    {
+
+        ImageSource StarFill, StarHalf, StarBorder;
+
+        public float StarValue { get; private set; }
+
+        public StarTemplate()
+        {
+            InitializeComponent();
+            StarFill =  ImageSource.FromFile("star_black.png");
+            StarHalf = ImageSource.FromFile("star_half.png");
+            StarBorder = ImageSource.FromFile("star_border.png");
+            StarValue = 0f;
+        }
+
+        private void SetStar(object sender, EventArgs e)
+        {
+
+
+            Frame frame = (Frame)sender;
+            Grid grid = (Grid)frame.Parent;
+
+           
+            //Debug.WriteLine(grid.Children.IndexOf((Frame)sender));
+
+            switch (grid.Children.IndexOf(frame))
+            {
+                case 5:
+                    Star1.Source = StarHalf;
+                    Star2.Source = StarBorder;
+                    Star3.Source = StarBorder;
+                    Star4.Source = StarBorder;
+                    Star5.Source = StarBorder;
+                    StarValue = 0.5f;
+                    break;
+                case 6:
+                    Star1.Source = StarFill;
+                    Star2.Source = StarBorder;
+                    Star3.Source = StarBorder;
+                    Star4.Source = StarBorder;
+                    Star5.Source = StarBorder;
+                    StarValue = 1f;
+                    break;
+
+                case 7:
+                    Star1.Source = StarFill;
+                    Star2.Source = StarHalf;
+                    Star3.Source = StarBorder;
+                    Star4.Source = StarBorder;
+                    Star5.Source = StarBorder;
+                    StarValue = 1.5f;
+                    break;
+                case 8:
+                    Star1.Source = StarFill;
+                    Star2.Source = StarFill;
+                    Star3.Source = StarBorder;
+                    Star4.Source = StarBorder;
+                    Star5.Source = StarBorder;
+                    StarValue = 2f;
+                    break;
+
+                case 9:
+                    Star1.Source = StarFill;
+                    Star2.Source = StarFill;
+                    Star3.Source = StarHalf;
+                    Star4.Source = StarBorder;
+                    Star5.Source = StarBorder;
+                    StarValue = 2.5f;
+                    break;
+                case 10:
+                    Star1.Source = StarFill;
+                    Star2.Source = StarFill;
+                    Star3.Source = StarFill;
+                    Star4.Source = StarBorder;
+                    Star5.Source = StarBorder;
+                    StarValue = 3f;
+                    break;
+
+                case 11:
+                    Star1.Source = StarFill;
+                    Star2.Source = StarFill;
+                    Star3.Source = StarFill;
+                    Star4.Source = StarHalf;
+                    Star5.Source = StarBorder;
+                    StarValue = 3.5f;
+                    break;
+                case 12:
+                    Star1.Source = StarFill;
+                    Star2.Source = StarFill;
+                    Star3.Source = StarFill;
+                    Star4.Source = StarFill;
+                    Star5.Source = StarBorder;
+                    StarValue = 4f;
+                    break;
+
+                case 13:
+                    Star1.Source = StarFill;
+                    Star2.Source = StarFill;
+                    Star3.Source = StarFill;
+                    Star4.Source = StarFill;
+                    Star5.Source = StarHalf;
+                    StarValue = 4.5f;
+                    break;
+                case 14:
+                    Star1.Source = StarFill;
+                    Star2.Source = StarFill;
+                    Star3.Source = StarFill;
+                    Star4.Source = StarFill;
+                    Star5.Source = StarFill;
+                    StarValue = 5f;
+                    break;
+
+                default:
+                    break;
+            }
+
+
+        }
+    }
+}
