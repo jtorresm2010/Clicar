cd884025b4d0fba92df2a8576a1fd7c8a1c5f9c3
jtorresm2010 <jtorresm2010@gmail.com>
2019-08-28 16:12:13 -0400

    Implementado Accordion localmente

---
+++ Clicar/Clicar/Customs/CustomAccordion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.CustomControls;
+
+namespace Clicar.Customs
+{
+    public class CustomAccordion : AccordionRepeaterView
+    {
+        
+    }
+}
